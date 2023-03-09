fa523ee12b0a9b99afc76adf391160d17dabe788
cyj <943133084@qq.com>
2023-03-09 14:25:06 +0800

    add I/O

---
+++ IO/listargs.c
@@ -0,0 +1,13 @@
+/*listargs.c
+ *				print the number of command line args,list the args,
+ *		then print a message
+ */
+#include <stdio.h>
+int main(int ac,char*av[])
+{
+	int i;
+	printf("Number of args: %d,Args are: \n",ac);
+	for(i = 0;i < ac;i++)
+		printf("args[%d] %s\n",i,av[i]);
+	fprintf(stderr,"This message is sent to stderr");
+}
